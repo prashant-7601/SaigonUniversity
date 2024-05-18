092d4e76f1a60813e5bbe5736470c3c162d2ad82
prashantsuntel <111524783+prashantsuntel@users.noreply.github.com>
2024-05-19 00:21:43 +0530

    added not ready yet page

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -4,7 +4,7 @@ export const ErrorPage = () => {
   const error: any = useRouteError();
 
   return (
-    <div id="error-page">
+    <div className="error-page d-flex flex-column align-items-center text-center justify-content-center">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
